10a858a44a3410ad350f952de07b770daa23a73e
Pedro Ayres <pedroayres@live.com>
2020-11-19 15:29:10 -0300

    Adicionado config.js

---
+++ aula_06/exercises/backend-facebook-login/configuration/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  /*
+   * This file contains the configurations information of Twitter login app.
+   * It consists of Twitter app information, database information.
+   */
+
+  facebook_api_key: "",
+  facebook_api_secret: "",
+  callback_url: "http://localhost:4000/auth/facebook/callback",
+  use_database: false,
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: ""
+};
